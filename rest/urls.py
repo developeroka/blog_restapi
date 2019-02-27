c9cd1ae167b155fd3fa9c9097df4831c6d036d72
developeroka <developeroka@gmail.com>
2019-02-27 13:02:01 +0330

    change type of input parameters in get posts requests

--- rest/urls.py
+++ rest/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
 from django.urls import path
 from rest import views
 
 
 urlpatterns = [
-    path('api', views.RestApi.api),
-    path('api/category', views.RestApi.categories),
+    path('api', views.ApiEndpoints.api),
+    path('api/category', views.ApiEndpoints.categories),
     path('register', views.UserActivity.register),
-    path('login', views.UserActivity.login)
+    path('login', views.UserActivity.login),
+    path('api/getpostbyid', views.ApiEndpoints.get_post_by_id),
+    path('api/getpostsbylength', views.ApiEndpoints.get_posts_by_length),
+    path('api/deletepostbyid', views.ApiEndpoints.delete_post_by_id)
 ]
--- rest/views.py
+++ rest/views.py
@@ -12,57 +12,79 @@ from django.utils import timezone
 import re
 
 
-class RestApi:
+class ApiEndpoints:
+
     @csrf_exempt
     def api(request):
-        token_availability = RestApi.check_token_availability(request)
+        token_availability = ApiFunctions.check_token_availability(request)
         result = token_availability['result']
         status = token_availability['status']
         if result is True:
             token_id = token_availability['available_token']
             if request.method == 'GET':
-                return RestApi.get(request, token_id)
+                return ApiFunctions.get(request, token_id)
 
             elif request.method == 'POST':
-                return RestApi.post(request, token_id)
+                return ApiFunctions.post(request, token_id)
 
             elif request.method == 'PUT':
-                return RestApi.put(request)
+                return ApiFunctions.put(request)
 
-            elif request.method == 'DELETE':
-                return RestApi.delete(request)
             else:
                 return JsonResponse({'result': 'you can\'t send this type of request'}, status=400)
         else:
             return JsonResponse({'result': result}, status=status)
 
-    def get(request, token_id):
-        post_id = request.GET.get('post_id')
-        available_token = ApiToken.objects.get(id=token_id)
-        if post_id:
-            user = available_token.token_user
-            post_query = BlogPost.objects.filter(id=post_id)
-            if post_query.first():
-                if post_query.first().post_author == user or \
-                        post_query.first().post_privacy == 'public':
-                    data = {'post_title': post_query.first().post_title,
-                            'post_content': post_query.first().post_content,
-                            'post_id': post_query.first().id,
-                            'post_author': post_query.first().post_author.username,
-                            'post_category': post_query.first().post_category.category_name,
-                            'post_privacy': post_query.first().post_privacy
-                            }
-                    response = JsonResponse(data, status=200)
-                    return response
+    @csrf_exempt
+    def get_post_by_id(request):
+        token_availability = ApiFunctions.check_token_availability(request)
+        result = token_availability['result']
+        status = token_availability['status']
+        if result is True:
+            token_id = token_availability['available_token']
+            if request.method == 'GET':
+                post_id = request.GET.get('post_id')
+                available_token = ApiToken.objects.get(id=token_id)
+                if post_id:
+                    user = available_token.token_user
+                    post_query = BlogPost.objects.filter(id=post_id)
+                    if post_query.first():
+                        if post_query.first().post_author == user or \
+                           post_query.first().post_privacy == 'public':
+                            data = {'post_title': post_query.first().post_title,
+                                    'post_content': post_query.first().post_content,
+                                    'post_id': post_query.first().id,
+                                    'post_author': post_query.first().post_author.username,
+                                    'post_category': post_query.first().post_category.category_name,
+                                    'post_privacy': post_query.first().post_privacy
+                                    }
+                            response = JsonResponse(data, status=200)
+                            return response
+                        else:
+                            data = {'Error message:': 'you\'re not able to see this post'}
+                            return JsonResponse(data, status=403)
+                    else:
+                        data = {'Error message:': 'this post is not valid'}
+                        return JsonResponse(data, status=404)
                 else:
-                    data = {'Error message:': 'you\'re not able to see this post'}
-                    return JsonResponse(data, status=403)
+                    data = {'Error message:': 'send wanted post id'}
+                    return JsonResponse(data, status=400)
             else:
-                data = {'Error message:': 'this post is not valid'}
-                return JsonResponse(data, status=404)
+                data = {'Error message:': 'You can\'t request more than 5 items'}
+                return JsonResponse(data, status=416)
         else:
-            first_item = request.GET.get('first_item') or None
-            last_item = request.GET.get('last_item') or None
+            return JsonResponse(result, status=status)
+
+    @csrf_exempt
+    def get_posts_by_length(request):
+        token_availability = ApiFunctions.check_token_availability(request)
+        result = token_availability['result']
+        status = token_availability['status']
+        first_item = request.GET.get('first_item') or None
+        last_item = request.GET.get('last_item') or None
+        if result is True:
+            token_id = token_availability['available_token']
+            available_token = ApiToken.objects.get(id=token_id)
             if first_item and last_item:
                 try:
                     first_numb = int(first_item)
@@ -70,28 +92,121 @@ class RestApi:
 
                 except Exception as e:
                     return JsonResponse({'result': str(e)}, status=400)
-            else:
-                first_numb = 1
-                last_numb = 5
 
-            item_delta = last_numb - first_numb
-            if item_delta < 5:
-                user = available_token.token_user
-                post_query = BlogPost.objects.filter(
-                    Q(post_privacy='public') |
-                    Q(post_author=user)
-                )[first_numb:last_numb]
-                data = [{'post_title': a.post_title,
-                         'post_content': a.post_content,
-                         'post_author': a.post_author.username,
-                         'post_id': a.pk,
-                         'post_privacy': str(a.post_privacy),
-                         'post_category': a.post_category.category_name
-                         } for a in post_query]
+                item_delta = last_numb - first_numb
+                if item_delta < 5:
+                    user = available_token.token_user
+                    post_query = BlogPost.objects.filter(
+                        Q(post_privacy='public') |
+                        Q(post_author=user)
+                    )[first_numb:last_numb]
+                    data = [{'post_title': a.post_title,
+                             'post_content': a.post_content,
+                             'post_author': a.post_author.username,
+                             'post_id': a.pk,
+                             'post_privacy': str(a.post_privacy),
+                             'post_category': a.post_category.category_name
+                             } for a in post_query]
+                    return JsonResponse({'data': data}, status=200)
+                else:
+                    data = {'Error message:': 'You can\'t request more than 5 items'}
+                    return JsonResponse(data, status=416)
+            else:
+                data = {'Error message:': 'send first_item and last_item with your request'}
+                return JsonResponse(data, status=416)
+        else:
+            return JsonResponse({'result': result}, status=status)
+
+    @csrf_exempt
+    def delete_post_by_id(request):
+        token_availability = ApiFunctions.check_token_availability(request)
+        result = token_availability['result']
+        status = token_availability['status']
+        if request.method == 'DELETE':
+            if result is True:
+                post_id = request.GET.get('post_id')
+                if post_id:
+                    post = BlogPost.objects.filter(id=post_id).first()
+                    if post:
+                        deleted_id = post.id
+                        post.delete()
+                        data = {'result': 'The \' ' + str(deleted_id) + ' \'' +
+                                          ' post has been deleted successfully. '}
+                        return JsonResponse(data, status=200)
+                    else:
+                        data = {'result': 'insert a valid post id'}
+                        return JsonResponse(data, status=404)
+                else:
+                    data = {'result': 'insert a post id'}
+                    return JsonResponse(data, status=400)
+            else:
+                return JsonResponse({'result': result}, status=status)
+        else:
+            return JsonResponse({'result': 'You can\'t send this type of request'}, status=400)
+
+    @csrf_exempt
+    def categories(request):
+        if request.method == 'GET':
+            token_availability = ApiFunctions.check_token_availability(request)
+            result = token_availability['result']
+            status = token_availability['status']
+            if result is True:
+                category_query = PostCategory.objects.all()
+                data = [{
+                    'category_title': cat.category_name,
+                    'category_id': cat.id
+                } for cat in category_query]
                 return JsonResponse({'data': data}, status=200)
             else:
-                data = {'Error message:': 'You can\'t request more than 5 items'}
-                return JsonResponse(data, status=416)
+                return JsonResponse({'result': result}, status=status)
+        else:
+            return JsonResponse({'result': 'you can\'t send this type of request'}, status=400)
+
+
+class ApiFunctions:
+
+    def check_token_availability(request):
+        sent_token = request.META.get('HTTP_AUTHORIZATION')
+        if sent_token is not None:
+            res_token = ApiToken.objects.filter(token_content=sent_token)
+            if res_token.first():
+                available_token = res_token.first()
+                token_expired = available_token.token_expired
+                if datetime.strptime(str(token_expired)[:19], "%Y-%m-%d %H:%M:%S") > datetime.now():
+                    time_difference = timedelta(minutes=5)
+                    token_expired += time_difference
+                    available_token.token_expired = token_expired
+                    available_token.save()
+                    data = {'result': True, 'available_token': available_token.id, 'status': 200}
+                    return data
+                else:
+                    data = {'result': 'Your token has been expired, '
+                                      'you must login again for new access token.', 'status': 400}
+                    return data
+            else:
+                data = {'result': 'not available Token', 'status': 404}
+                return data
+        else:
+            data = {'result': 'please send your token with your request', 'status': 401}
+            return data
+
+    def get(request, token_id):
+        available_token = ApiToken.objects.get(id=token_id)
+        first_numb = 1
+        last_numb = 5
+        user = available_token.token_user
+        post_query = BlogPost.objects.filter(
+            Q(post_privacy='public') |
+            Q(post_author=user)
+        )[first_numb:last_numb]
+        data = [{'post_title': a.post_title,
+                 'post_content': a.post_content,
+                 'post_author': a.post_author.username,
+                 'post_id': a.pk,
+                 'post_privacy': str(a.post_privacy),
+                 'post_category': a.post_category.category_name
+                 } for a in post_query]
+        return JsonResponse({'data': data}, status=200)
 
     def post(request, token_id):
         available_token = ApiToken.objects.get(id=token_id)
@@ -160,7 +275,7 @@ class RestApi:
                         current_post.save()
                     else:
                         data = {'result': 'insert available category'}
-                        return JsonResponse(data, status=40)
+                        return JsonResponse(data, status=400)
                 elif title and category and content is None:
                     data = {'result': 'insert your editing field'}
                     return JsonResponse(data, status=400)
@@ -175,67 +290,6 @@ class RestApi:
             data = {'result': 'insert a post id'}
             return JsonResponse(data, status=400)
 
-    def delete(request):
-        post_id = request.GET.get('post_id')
-        if post_id:
-            post = BlogPost.objects.filter(id=post_id).first()
-            if post:
-                deleted_id = post.id
-                post.delete()
-                data = {'result': 'The \' ' + str(deleted_id) + ' \'' +
-                                  ' post has been deleted successfully. '}
-                return JsonResponse(data, status=200)
-            else:
-                data = {'result': 'insert a valid post id'}
-                return JsonResponse(data, status=404)
-        else:
-            data = {'result': 'insert a post id'}
-            return JsonResponse(data, status=400)
-
-    def check_token_availability(request):
-        sent_token = request.META.get('HTTP_AUTHORIZATION')
-        if sent_token is not None:
-            req_token = sent_token.split(' ')[1]
-            res_token = ApiToken.objects.filter(token_content=req_token)
-            if res_token.first():
-                available_token = res_token.first()
-                token_expired = available_token.token_expired
-                if datetime.strptime(str(token_expired)[:19], "%Y-%m-%d %H:%M:%S") > datetime.now():
-                    time_difference = timedelta(minutes=5)
-                    token_expired += time_difference
-                    available_token.token_expired = token_expired
-                    available_token.save()
-                    data = {'result': True, 'available_token': available_token.id, 'status': 200}
-                    return data
-                else:
-                    data = {'result': 'Your token has been expired, '
-                                      'you must login again for new access token.', 'status': 400}
-                    return data
-            else:
-                data = {'result': 'not available Token', 'status': 404}
-                return data
-        else:
-            data = {'result': 'please send your token with your request', 'status': 401}
-            return data
-
-    @csrf_exempt
-    def categories(request):
-        if request.method == 'GET':
-            token_availability = RestApi.check_token_availability(request)
-            result = token_availability['result']
-            status = token_availability['status']
-            if result is True:
-                category_query = PostCategory.objects.all()
-                data = [{
-                    'category_title': cat.category_name,
-                    'category_id': cat.id
-                } for cat in category_query]
-                return JsonResponse({'data': data}, status=200)
-            else:
-                return JsonResponse({'result': result}, status=status)
-        else:
-            return JsonResponse({'result': 'you can\'t send this type of request'}, status=400)
-
 
 class UserActivity:
 
@@ -244,7 +298,6 @@ class UserActivity:
 
     @csrf_exempt
     def register(request):
-
         if request.method == 'POST':
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
@@ -252,42 +305,44 @@ class UserActivity:
             password = body.get('password')
             username_checking = Utilities.RegEx.check_username_matching(username)
             password_checking = Utilities.RegEx.check_password_matching(password)
+            if username and password is not None:
+                if username_checking is True:
+                    if password_checking is True:
+                        receive_username = User.objects.all().filter(Q(username=username)).first()
+                        if receive_username is None:
+                            new_user = User(username=username, password=password)
+                            new_user.save()
+                            user = User.objects.get(username=username)
+                            time_difference = timedelta(minutes=5)
+                            expire_date = timezone.now() + time_difference
 
-            if username_checking is True:
-                if password_checking is True:
-                    receive_username = User.objects.all().filter(Q(username=username)).first()
-                    if receive_username is None:
-                        new_user = User(username=username, password=password)
-                        new_user.save()
-                        user = User.objects.get(username=username)
-                        time_difference = timedelta(minutes=5)
-                        expire_date = timezone.now() + time_difference
+                            """TODO : check why token/user _form doesn't have
+                             cleaned_data without breakpoint but it has with it...!"""
 
-                        """TODO : check why token/user _form doesn't have
-                         cleaned_data without breakpoint but it has with it...!"""
-
-                        my_token = salted_hmac(user, datetime.now()).hexdigest()
-                        token = ApiToken(token_content=my_token,
-                                         token_expired=expire_date,
-                                         token_user=user)
-                        token.save()
-                        return JsonResponse({
-                             'token: ': token.token_content,
-                             'expired: ':  token.token_expired,
-                        }, status=200)
+                            my_token = salted_hmac(user, datetime.now()).hexdigest()
+                            token = ApiToken(token_content=my_token,
+                                             token_expired=expire_date,
+                                             token_user=user)
+                            token.save()
+                            return JsonResponse({
+                                 'token: ': token.token_content,
+                                 'expired: ':  token.token_expired,
+                            }, status=200)
+                        else:
+                            return JsonResponse({'result': 'username is already exists'}, status=400)
                     else:
-                        return JsonResponse({'result': 'username is already exists'}, status=400)
+                        return JsonResponse({'result': password_checking['Err']}, status=400)
                 else:
-                    return JsonResponse({'result': password_checking['Err']}, status=400)
+                    return JsonResponse({'result': username_checking['Err']}, status=400)
             else:
-                return JsonResponse({'result': username_checking['Err']}, status=400)
+                return JsonResponse({'result': 'Enter required fields '
+                                               '(username, password)'}, status=400)
 
         else:
             return JsonResponse({'result': 'You can\'t send this type of request'}, status=403)
 
     @csrf_exempt
     def login(request):
-
         if request.method == 'POST':
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
